cb5257cd8110a39208d13681f24013990a4dd13b
tahina ramanirabahoaka <tahina.ramanirabahoaka@epitech.eu>
2015-09-07 17:37:34 +0200

    fix image upload

---
+++ Spont/app/src/main/java/Task/FileUploaderTask.java
@@ -0,0 +1,140 @@
+package Task;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.provider.MediaStore;
+import android.webkit.WebView;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+import model.Globals;
+
+/**
+ * Created by ramani on 07/09/2015.
+ */
+public class FileUploaderTask extends AsyncTask<String, String, String> {
+    private Context mContext;
+    private Uri pathFile;
+    private WebView mWebView;
+
+    public FileUploaderTask(Context mContext_, Uri pathFile_, WebView mWebView_) {
+        this.mContext = mContext_;
+        this.pathFile = pathFile_;
+        this.mWebView = mWebView_;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        this.mWebView.loadUrl("javascript:loadingAnimation()");
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        System.out.println(Globals.offerId);
+        HttpClient client = new DefaultHttpClient();
+        File imageFile = new File(getRealPathFromURI(this.mContext, this.pathFile));
+        System.out.println(imageFile.getAbsolutePath());
+        HttpPost postRequest = new HttpPost ("http://app.spont.fr/upload_image/"+Globals.offerId);
+        MultipartEntity multiPartEntity = new MultipartEntity () ;
+        try {
+            multiPartEntity.addPart("action", new StringBody("save-image")) ;
+            multiPartEntity.addPart("mobilePhone", new StringBody(Globals.mobilePhone)) ;
+            multiPartEntity.addPart("password", new StringBody(Globals.password)) ;
+            multiPartEntity.addPart("android_exception", new StringBody("1")) ;
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        FileBody fileBody = new FileBody(imageFile, imageFile.getName(), "image/jpeg","UTF-8") ;
+        multiPartEntity.addPart("image", fileBody) ;
+        postRequest.setEntity(multiPartEntity) ;
+        return executeRequest(postRequest);
+    }
+    private String executeRequest(HttpRequestBase requestBase){
+        String responseString = "" ;
+        InputStream responseStream = null ;
+        HttpClient client = new DefaultHttpClient () ;
+        try{
+            HttpResponse response = client.execute(requestBase) ;
+            if (response != null){
+                HttpEntity responseEntity = response.getEntity() ;
+
+                if (responseEntity != null){
+                    responseStream = responseEntity.getContent() ;
+                    if (responseStream != null){
+                        BufferedReader br = new BufferedReader (new InputStreamReader (responseStream)) ;
+                        String responseLine = br.readLine() ;
+                        System.out.println(responseLine);
+                        String tempResponseString = "" ;
+                        while (responseLine != null){
+                            tempResponseString = tempResponseString + responseLine + System.getProperty("line.separator") ;
+                            responseLine = br.readLine() ;
+                        }
+                        br.close() ;
+                        if (tempResponseString.length() > 0){
+                            responseString = tempResponseString;
+                            return responseString;
+                        }
+                    }
+                }
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            if (responseStream != null){
+                try {
+                    responseStream.close() ;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        client.getConnectionManager().shutdown();
+        return null;
+    }
+
+    public String getRealPathFromURI(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String urlImage) {
+        super.onPostExecute(urlImage);
+        this.mWebView.loadUrl("javascript:changeThumb('"+urlImage+"')");
+    }
+}
---
+++ Spont/app/src/main/java/model/Globals.java
@@ -0,0 +1,10 @@
+package model;
+
+/**
+ * Created by ramani on 07/09/2015.
+ */
+public class Globals {
+    public static String offerId;
+    public static String mobilePhone;
+    public static String password;
+}
